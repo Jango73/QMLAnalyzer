f232e7c0eea88cbea1390f76c1e5d1f844b6848c
Jango73 <Jango73@users.noreply.github.com>
2019-06-11 07:51:10 +0200

    Update CFolderView.h

--- QMLAnalyzer/cpp/Views/CFolderView.h
+++ QMLAnalyzer/cpp/Views/CFolderView.h
@@ -1,16 +1,3 @@
-/*******************************************************************************
-* P. C. A.
-*
-* Peugeot Citroen Automobiles
-*
-* This file is the property of PCA. All rights are reserved
-* by PCA and this file must not be copied or disclosed
-* (in whole or in part) without prior written consent of PCA.
-*
-* Concept: Raphaël Drap
-* Authors: Guillaume Darier, Franck Legall
-*
-*******************************************************************************/
 
 #pragma once
 
